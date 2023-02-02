030484d50eb6b66aae18ad84a4c0becf7fe8a4d9
Timetoslide23 <124249186+Timetoslide23@users.noreply.github.com>
2023-02-02 11:05:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function submitAnswers() {
   var score = 0;
         
   // Prendi le risposte alle domande
-  var q1 = document.namedItem("question1").value;
-  var q2 = document.namedItem("question2").value;
-  var q3 = document.namedItem("question3").value;
-  var q4 = document.namedItem("question4").value;
-  var q5 = document.namedItem("question5").value;
+  var q1 = document.getElementById("quizForm").elements.question1.value;
+  var q2 = document.getElementById("quizForm").elements.question2.value;
+  var q3 = document.getElementById("quizForm").elements.question3.value;
+  var q4 = document.getElementById("quizForm").elements.question4.value;
+  var q5 = document.getElementById("quizForm").elements.question5.value;
         
   // Controlla se le risposte sono corrette
   if (q1 === "true") {
